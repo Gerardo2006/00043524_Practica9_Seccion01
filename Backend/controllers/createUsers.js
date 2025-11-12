aab5623b33a5923bb1b7c740c7912a17c1de5848
Gerardo2006 <164081228+Gerardo2006@users.noreply.github.com>
2025-11-11 23:17:30 -0600

    fix: Error para post de usuarios

--- Backend/controllers/createUsers.js
+++ Backend/controllers/createUsers.js
@@ -3,7 +3,7 @@ import { pool } from "../data/conection.js";
 export const createUser = (request, response) => {
   const { name, email, passwd } = request.body
 
-  pool.query('INSERT INTO users (name, email, passwd) VALUES ($1, $2, $3) RETURNING *', [name, email, passwd], (error, results) => {
+  pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, passwd], (error, results) => {
     if (error) {
       throw error
     }
